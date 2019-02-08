c052c1a6d063d4c9d6653615d0bc23d6bbc12ca5
David Mulvihill <dmulv@davids-macbook-air.local>
2019-02-07 21:14:04 -0500

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by David Mulvihill on 2/7/19.
+//  Copyright © 2019 David Mulvihill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
